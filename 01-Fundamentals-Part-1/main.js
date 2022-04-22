2b81389d5b6efbae008cf946859d02ddecc2ccc5
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-04-21 21:08:49 -0300

    lecture: variables + assignment

--- 01-Fundamentals-Part-1/main.js
+++ 01-Fundamentals-Part-1/main.js
@@ -1,7 +1,20 @@
 let js = "amazing";
-
-if (js === "amazing") {
-  alert("Javascript is FUN!");
-}
-
 console.log(40 + 8 + 23 + 10);
+console.log("Jonas");
+
+// variable declaration
+let firstName = "Lucas";
+console.log(firstName);
+
+// Part 1 Assignments:
+let country = "Brazil";
+let continent = "South America";
+let population = "200M";
+console.log(
+  "country: " +
+    country +
+    ", continent: " +
+    continent +
+    " - population: " +
+    population
+);
