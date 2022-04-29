b15fa84fb383f8c842d1669598c5ac2bcb703d2c
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-04-28 23:00:43 -0300

    study if else statements

---
+++ 01-Fundamentals-Part-1/decisionMaking.js
@@ -0,0 +1,20 @@
+const age = 15
+const oldEnough = age >= 18
+
+if(oldEnough){
+    console.log("you are allowed to have a driver's license 😎")
+} else {
+    console.log("try again in a few years 😅")
+}
+
+// WIN + . to add emojis
+
+const birthYear = 1991;
+let century;
+
+if (birthYear <= 2000) {
+     century = 20;
+} else {
+    century = 21;
+}
+console.log(century)
