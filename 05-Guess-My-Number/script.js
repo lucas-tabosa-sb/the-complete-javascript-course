8f17a0d570c723dcf1dd38244806cf1812b83c2a
Lucas Tabosa <104172470+lucas-tabosa-sb@users.noreply.github.com>
2022-05-18 21:49:47 -0300

    finishes implementing the game and first refactor

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -14,9 +14,13 @@ let score = 20
 let highScore = 0
 
 inputBtn.addEventListener("click", function () {
+	let guess = Number(inputGuess.value)
+
+	// if input field is empty
 	if (!inputGuess.value) {
 		statusMsg.textContent = "⛔ Insert a Number"
-	} else if (Number(inputGuess.value) === secretNumber) {
+		// if the guess is correct
+	} else if (guess === secretNumber) {
 		statusMsg.textContent = "Correct, you win!"
 		body.style.backgroundColor = "#60b347"
 		document.querySelector(".number").style.width = "30rem"
@@ -24,20 +28,10 @@ inputBtn.addEventListener("click", function () {
 		if (score > highScore) {
 			highScoreMsg.textContent = `🥇 Highscore: ${score}`
 		}
-	} else if (Number(inputGuess.value) < secretNumber) {
+		// if the guess is incorrect
+	} else if (guess !== secretNumber) {
 		if (score > 1) {
-			statusMsg.textContent = "too low!"
-			score--
-			labelScore.textContent = score
-		} else {
-			statusMsg.textContent = "You lost the Game 😭"
-			labelScore.textContent = 0
-			body.style.backgroundColor = "#e40505cc"
-			randomNumberDisplay.textContent = secretNumber
-		}
-	} else if (Number(inputGuess.value) > secretNumber) {
-		if (score > 1) {
-			statusMsg.textContent = "too high!"
+			statusMsg.textContent = guess < secretNumber ? "too low!" : "too high!"
 			score--
 			labelScore.textContent = score
 		} else {
@@ -51,6 +45,7 @@ inputBtn.addEventListener("click", function () {
 	}
 })
 
+// resets the game, keeping the highscore
 resetBtn.addEventListener("click", function () {
 	score = 20
 	secretNumber = Math.trunc(Math.random() * 20) + 1
@@ -63,5 +58,5 @@ resetBtn.addEventListener("click", function () {
 	console.log(secretNumber)
 })
 
-console.log(secretNumber)
-console.log(typeof secretNumber)
+//TODO
+// refactor even more, using functions for repetitive code
