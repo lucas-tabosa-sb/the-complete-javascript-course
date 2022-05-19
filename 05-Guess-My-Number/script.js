8939043936ebd0aecca244df7c25e3fcb7b43af1
Lucas Tabosa <104172470+lucas-tabosa-sb@users.noreply.github.com>
2022-05-18 21:35:54 -0300

    implements the reset buton and highscore

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -6,9 +6,12 @@ let inputGuess = document.querySelector(".guess")
 let inputBtn = document.querySelector(".check")
 let labelScore = document.querySelector(".score")
 let body = document.querySelector("body")
+let resetBtn = document.querySelector(".again")
+let highScoreMsg = document.querySelector(".label-highscore")
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1
 let score = 20
+let highScore = 0
 
 inputBtn.addEventListener("click", function () {
 	if (!inputGuess.value) {
@@ -18,6 +21,9 @@ inputBtn.addEventListener("click", function () {
 		body.style.backgroundColor = "#60b347"
 		document.querySelector(".number").style.width = "30rem"
 		randomNumberDisplay.textContent = secretNumber
+		if (score > highScore) {
+			highScoreMsg.textContent = `🥇 Highscore: ${score}`
+		}
 	} else if (Number(inputGuess.value) < secretNumber) {
 		if (score > 1) {
 			statusMsg.textContent = "too low!"
@@ -43,6 +49,18 @@ inputBtn.addEventListener("click", function () {
 	} else {
 		console.log("test")
 	}
+})
+
+resetBtn.addEventListener("click", function () {
+	score = 20
+	secretNumber = Math.trunc(Math.random() * 20) + 1
+	statusMsg.textContent = "Start Guessing..."
+	inputGuess.value = ""
+	labelScore.textContent = score
+	randomNumberDisplay.textContent = "?"
+	body.style.backgroundColor = "#222"
+	document.querySelector(".number").style.width = "15rem"
+	console.log(secretNumber)
 })
 
 console.log(secretNumber)
