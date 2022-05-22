e717ff56d159303df928aaaafb1c8a3c9236b5cc
Lucas Tabosa <104172470+lucas-tabosa-sb@users.noreply.github.com>
2022-05-22 16:03:53 -0300

    studying scopes

---
+++ 08-Behind-The-Scenes/script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// global scope
+function calcAge(birthYear) {
+	const age = 2022 - birthYear // age and birthYear are accessible by any function inside the same scope
+	console.log(firstName) // not in function scope, but in global scope, so it will work
+
+	// function scope -> scope chain lookup
+	function printAge() {
+		const output = `you are ${age}, born in ${birthYear}`
+		console.log(output)
+	}
+
+	printAge()
+
+	return age
+}
+
+//global variable
+const firstName = "Lucas"
+
+calcAge(1991)
