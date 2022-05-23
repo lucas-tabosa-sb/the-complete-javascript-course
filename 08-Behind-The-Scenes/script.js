c67dfdc96f0c9e3468d52fc95c7b0c30bb7876c4
Lucas Tabosa <104172470+lucas-tabosa-sb@users.noreply.github.com>
2022-05-22 21:05:34 -0300

    edit script js

--- 08-Behind-The-Scenes/script.js
+++ 08-Behind-The-Scenes/script.js
@@ -1,22 +1,32 @@
 "use strict"
 
-// global scope
-function calcAge(birthYear) {
-	const age = 2022 - birthYear // age and birthYear are accessible by any function inside the same scope
-	console.log(firstName) // not in function scope, but in global scope, so it will work
+// // global scope
+// function calcAge(birthYear) {
+// 	const age = 2022 - birthYear // age and birthYear are accessible by any function inside the same scope
+// 	console.log(firstName) // not in function scope, but in global scope, so it will work
 
-	// function scope -> scope chain lookup
-	function printAge() {
-		const output = `you are ${age}, born in ${birthYear}`
-		console.log(output)
-	}
+// 	// function scope -> scope chain lookup
+// 	function printAge() {
+// 		const output = `you are ${age}, born in ${birthYear}`
+// 		console.log(output)
+// 	}
 
-	printAge()
+// 	printAge()
 
-	return age
-}
+// 	return age
+// }
 
-//global variable
-const firstName = "Lucas"
+// //global variable
+// const firstName = "Lucas"
 
-calcAge(1991)
+// calcAge(1991)
+
+// HOISTING AND TDZ
+//
+// console.log(me)
+// console.log(job)
+// console.log(year)
+
+// var me = "Lucas"
+// let job = "tech trainer"
+// const year = 1991
