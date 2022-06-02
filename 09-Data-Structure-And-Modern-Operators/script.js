21130db27a8a6e1a63e4f34b95362bbc619e3be6
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-01 22:53:39 -0300

    implements code challenge 2

--- 09-Data-Structure-And-Modern-Operators/script.js
+++ 09-Data-Structure-And-Modern-Operators/script.js
@@ -300,6 +300,98 @@ console.log(restaurant)
 // console.log(users[3]?.name ?? 'no array item at that index')
 
 // LOOPING OBJECTS
-for (const day of Object.keys(openingHours)){
-	console.log(day)
+// for (const day of Object.keys(openingHours)){
+// 	console.log(day)
+// }
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+	team1: 'Bayern Munich',
+	team2: 'Borrussia Dortmund',
+	players: [
+	  [
+		'Neuer',
+		'Pavard',
+		'Martinez',
+		'Alaba',
+		'Davies',
+		'Kimmich',
+		'Goretzka',
+		'Coman',
+		'Muller',
+		'Gnarby',
+		'Lewandowski',
+	  ],
+	  [
+		'Burki',
+		'Schulz',
+		'Hummels',
+		'Akanji',
+		'Hakimi',
+		'Weigl',
+		'Witsel',
+		'Hazard',
+		'Brandt',
+		'Sancho',
+		'Gotze',
+	  ],
+	],
+	score: '4:0',
+	scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+	date: 'Nov 9th, 2037',
+	odds: {
+	  team1: 1.33,
+	  x: 3.25,
+	  team2: 6.5,
+	},
+  };
+
+//   const keys = Object.keys(game.scored)
+//   console.log(keys)
+
+//   const values = Object.values(game.scored)
+//   console.log(values)
+
+//   const entries = Object.entries(game.scored)
+//   console.log(entries)
+
+  for (const [key, value] of game.scored.entries()){
+	  console.log(`Goal ${key + 1}: ${value}`)
+  }
+
+  const odds = Object.values(game.odds) // array with odds values
+  console.log(odds)
+  let average = 0
+  for (const odd of odds){
+	  average += odd
+	}
+	
+	console.log(`Odds average: ${(average/odds.length).toFixed(2)}`)
+
+for(const [team, odd] of Object.entries(game.odds)){
+	const teamStr = team === 'x' ? 'draw' : `Victory ${game[team]}`
+	console.log(`Odd of ${teamStr} is ${odd}`)
 }
