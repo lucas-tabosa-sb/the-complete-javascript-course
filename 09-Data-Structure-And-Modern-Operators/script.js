6a697aebaaf11f494a2f5425208eadfb7cb5e05a
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-05 18:55:10 -0300

    finishing work with strings

--- 09-Data-Structure-And-Modern-Operators/script.js
+++ 09-Data-Structure-And-Modern-Operators/script.js
@@ -582,3 +582,99 @@ const checkBaggage = function(items){
 checkBaggage('I have a laptop, some food and a knife')
 checkBaggage('socks and camera')
 checkBaggage('got some snacks and a gun for protection')
+
+//split and join
+console.log('a+very+nice+string'.split('+'))
+console.log('Jonas Schmedtmann'.split(' '))
+
+const [firstName, lastName] = 'Lucas Tabosa'.split(' ')
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ')
+console.log(newName)
+
+
+const capitalizaName = function(name){
+      const names = name.split(' ')
+      const namesUpper = []
+
+      for (const n of names){
+            namesUpper.push(n[0].toUpperCase() + n.slice(1))
+      }
+
+      console.log(namesUpper.join(' '))
+}
+
+capitalizaName('jessica ann smith davis')
+capitalizaName('lucas tabosa soares bulcão')
+
+// padding string
+const message = 'Go to gate 23!'
+console.log(message.padStart(25, '+').padEnd(40, '+'))
+
+const maskCreditCard = function(number){
+      const str = number + ''
+      const last = str.slice(-4)
+      return last.padStart(str.length, '*')
+}
+
+console.log(maskCreditCard(3128734684755897))
+const test = '3128734684755897'
+console.log(test.split(''))
+
+// repeat
+const message3 = 'Bad weather...All departures delayed... '
+console.log(message3.repeat(3))
+
+const planesInLine = function(n){
+      console.log(`there are ${n} planes in line ${'🛫'.repeat(n)}`)
+}
+
+planesInLine(5)
+planesInLine(2)
+planesInLine(20)
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function(){
+      const text = document.querySelector('textarea').value
+      const rows = text.split('\n')
+
+      for (const [i, row] of rows.entries()){
+            const [first, second] = row.toLowerCase().trim().split('_')
+
+            const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`
+      }
+      console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`)
+})
