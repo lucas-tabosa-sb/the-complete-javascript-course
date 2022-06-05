3cdbeb6419e4e9e1880eced8a355e5df44e2f847
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-05 17:14:09 -0300

    implements code challenge 3

--- 09-Data-Structure-And-Modern-Operators/script.js
+++ 09-Data-Structure-And-Modern-Operators/script.js
@@ -388,7 +388,7 @@ GOOD LUCK 😀
 //   for (const odd of odds){
 // 	  average += odd
 // 	}
-	
+
 // 	console.log(`Odds average: ${(average/odds.length).toFixed(2)}`)
 
 // for(const [team, odd] of Object.entries(game.odds)){
@@ -407,7 +407,6 @@ GOOD LUCK 😀
 // orderSet.delete('Garlic Bread')
 // console.log(orderSet)
 
-
 // console.log(new Set('Jonas'))
 
 // example
@@ -425,18 +424,64 @@ GOOD LUCK 😀
 // rest.set(2, 'Lisbon, Portugal')
 // console.log(rest)
 
-const question = new Map([
-	['question', 'What is the best programming language in the world?'],
-	[1, 'C#'],
-	[2, 'Java'],
-	[3, 'Javascript'],
-	['correct', 3],
-	[true, 'Correct'],
-	[false, 'Try again']
+// const question = new Map([
+// 	['question', 'What is the best programming language in the world?'],
+// 	[1, 'C#'],
+// 	[2, 'Java'],
+// 	[3, 'Javascript'],
+// 	['correct', 3],
+// 	[true, 'Correct'],
+// 	[false, 'Try again']
+// ])
+
+// console.log(question)
+
+// for(const [key, values] of question){
+// 	console.log(key, values)
+// }
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+	[17, "⚽️ GOAL"],
+	[36, "🔁 Substitution"],
+	[47, "⚽️ GOAL"],
+	[61, "🔁 Substitution"],
+	[64, "🔶 Yellow card"],
+	[69, "🔴 Red card"],
+	[70, "🔁 Substitution"],
+	[72, "🔁 Substitution"],
+	[76, "⚽️ GOAL"],
+	[80, "⚽️ GOAL"],
+	[92, "🔶 Yellow card"],
 ])
 
-console.log(question)
+console.log(gameEvents)
 
-for(const [key, values] of question){
-	console.log(key, values)
+//1.
+const events = [...new Set(gameEvents.values())]
+console.log(events)
+//2.
+gameEvents.delete(64)
+//3.
+const time = [...gameEvents.keys()].pop()
+console.log(time)
+console.log(`An event happened, on average, every ${time / gameEvents.size}`)
+//4.
+for(const [min, event] of gameEvents){
+      const half = min <=45 ? "FIRST" : "SECOND"
+      console.log(`[${half} HALF] ${min} : ${event}`)
 }
