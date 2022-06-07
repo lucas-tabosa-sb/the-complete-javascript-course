8d001c31186511139b4d1b12b1e07407d737f77f
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-06 23:01:48 -0300

    studies callbacks and higher order functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -48,3 +48,23 @@
 // newPassport(lucas)
 // checkIn(flight, lucas)
 
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase()
+}
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ')
+    return [first.toUpperCase(), ...others].join(' ')
+}
+
+// higher order function
+const transformer = function(str, fn){
+    console.log(`original string: ${str}`)
+    console.log(`transformed string: ${fn(str)}`)
+
+    console.log(`transformed by: ${fn.name}`)
+}
+
+transformer('javascript is the best', upperFirstWord)
+transformer('javascript is the best', oneWord)
+
