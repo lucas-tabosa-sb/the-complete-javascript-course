09d7bde6827a10f513f054da25c6d4bfebf4d76c
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-06 22:46:14 -0300

    studying funcions

---
+++ 10-Functions/script.js
@@ -0,0 +1,50 @@
+'use strict'
+
+// const bookings = []
+
+// const createBooking = function(flightNum, numPassengers = 1, price = 'USD $199') {
+
+//     // ES5 way to do:
+//     // numPassengers = numPassengers || 1
+//     // price = price || 'USD $100'
+
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price
+//     }
+//     console.log(booking)
+//     bookings.push(booking)
+// }
+
+// createBooking('LH123')
+// createBooking('LH123', 2, 800)
+
+// const flight = 'LH234'
+// const lucas = {
+//     name: 'Lucas Tabosa',
+//     passport: 123983748498
+// }
+
+// const checkIn = function(flightNumber, passenger){
+//     flightNumber = 'LH999'
+//     passenger.name = 'Mr. ' + passenger.name
+
+//     if(passenger.passport === 123983748498){
+//         alert('check in')
+//     } else {
+//         alert('Wrong passport')
+//     }
+// }
+
+// // checkIn(flight, lucas)
+// // console.log(flight)
+// // console.log(lucas)
+
+// const newPassport = function(person){
+//     person.passport = Math.trunc(Math.random() * 10000000000)
+// }
+
+// newPassport(lucas)
+// checkIn(flight, lucas)
+
