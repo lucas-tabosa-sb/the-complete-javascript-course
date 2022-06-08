526fc5c6d5d4883de445b96bd8d5b1fd29defed0
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-07 21:57:45 -0300

    studying apply and call methods

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -80,6 +80,45 @@
 // greeterHey('Steven')
 // greet('Hello')('Lucas')
 
-const greet = (greeting) => (personName) => console.log(`${greeting} ${personName}`) 
+// const greet = (greeting) => (personName) => console.log(`${greeting} ${personName}`)
+// greet('Hey')('Lucas')
 
-greet('Hey')('Lucas')
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book: function(flightNum, name){
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+    }
+}
+
+lufthansa.book(239, 'Lucas Tabosa')
+lufthansa.book(635, 'Jonas Schmedtmann')
+console.log(lufthansa)
+
+const eurowings = {
+    airline: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+}
+
+const book = lufthansa.book
+//does not work
+// book(23, 'Sarah Williams')
+
+// manipulates the this keyword with the call() function, making it so the this keyworkd points to the object that is passed in the first argument
+
+book.call(eurowings, 23, "Sarah Williams")
+console.log(eurowings)
+book.call(lufthansa, 123, "Mary Cooper")
+console.log(lufthansa)
+
+// apply method -> does the same as call() but not receiving arguments, receives an array, not so used anymore
+
+const flightData = [528, "George Cooper"]
+book.apply(eurowings, flightData)
+console.log(eurowings)
+// the same as apply -> spreading the array 
+book.call(eurowings, ...flightData)
+console.log(eurowings)
