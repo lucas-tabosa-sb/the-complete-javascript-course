8832bde7d59764b72ae84e97182b5631100ff830
Lucas Tabosa <lucas.tabosa.sb@gmail.com>
2022-06-13 22:31:33 -0300

    IIFE

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -203,31 +203,44 @@ BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 GOOD LUCK 😀
 */
 
-const poll = {
-    question: 'What is your favourite programming language',
-    options: ['0: Javascript','1: Python', '2: Rust', '3: C++'],
-    answers: new Array(4).fill(0),
-    registerNewAnswer(){
-        const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write the option number)`))
-        console.log(answer)
+// const poll = {
+//     question: 'What is your favourite programming language',
+//     options: ['0: Javascript','1: Python', '2: Rust', '3: C++'],
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer(){
+//         const answer = Number(prompt(`${this.question}\n${this.options.join('\n')}\n(Write the option number)`))
+//         console.log(answer)
 
-        typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
+//         typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
 
-        this.displayResults()
-        this.displayResults('string')
-    },
+//         this.displayResults()
+//         this.displayResults('string')
+//     },
 
-    displayResults(type = 'array'){
-        if(type === 'array'){
-            console.log(this.answers)
-        } else if (type === 'string'){
-            console.log(`Poll results are ${this.answers.join(', ')}`)
-        }
-    }
-}
+//     displayResults(type = 'array'){
+//         if(type === 'array'){
+//             console.log(this.answers)
+//         } else if (type === 'string'){
+//             console.log(`Poll results are ${this.answers.join(', ')}`)
+//         }
+//     }
+// }
 
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+// document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
 
-poll.displayResults.call({ answers: [5, 2, 3]}, 'string')
-poll.displayResults.call({ answers: [1, 5, 3, 9, 7, 1]}, 'string')
-poll.displayResults.call({ answers: [1, 5, 3, 9, 7, 1]})
+// poll.displayResults.call({ answers: [5, 2, 3]}, 'string')
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 7, 1]}, 'string')
+// poll.displayResults.call({ answers: [1, 5, 3, 9, 7, 1]})
+
+
+// Immediately Invoked Functions Expression
+
+// const runOnce = function(){
+//     console.log('this will never run again')
+// }
+
+// runOnce()
+
+(function(){
+    console.log('this will never run again')
+})()
